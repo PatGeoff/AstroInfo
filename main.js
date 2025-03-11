f6aa3e382df46f65a22ab011e2aa7bc17a4b1ffa
Jean-Maxime Couillard <jeanmaximecouillard@gmail.com>
2025-03-11 11:50:47 -0400

    Utilisation d'un templating engine (nunjucks) pour réduire la répétition dans le code.

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import express from "express";
 import path from "path";
 import fetchAll from './fetching.js';
 import parsing from './parsing.js';
+import nunjucks from "nunjucks";
 
 const ONE_DAY = 24 * 60 * 60 * 1000;
 const PORT = process.env.PORT || 3000;
@@ -11,12 +12,26 @@ export const app = express();
 // End-point to get local JSONs
 app.get('/data/:planet', parsing);
 
+// Enable nunjucks templating
+// https://mozilla.github.io/nunjucks/getting-started.html
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app,
+    // TODO Use environment variable for caching, and disable on production server
+    noCache: true
+});
+
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
 
+// Serve any page from the views folder
+app.get('/:page', (req, res) => {
+    res.render(req.params.page);
+});
+
 // Serve the index.html file at the root URL
 app.get('/', (req, res) => {
-    res.sendFile(path.join(process.cwd(), 'public', 'index.html'));
+    res.render('index.html');
 });
 
 // Start server
